1b33ce6323dfddb2194476a438f834fb56bb53de
Jarron Shih <jarronshih@gmail.com>
2013-11-25 09:52:17 +0800

    html2pdf modify

--- build.py
+++ build.py
@@ -12,25 +12,42 @@ HTML_TEMPLATE = """<!doctype html>
 </html>
 """
 
-def concatenate_file_path_array_to_string(file_path_ary):
-    content = ''
-    for file_name in file_path_ary:
-        file = open(file_name)
-        content += file.read() + '\n'
-    return content
+# def concatenate_file_path_array_to_string(file_path_ary):
+#     content = ''
+#     for file_name in file_path_ary:
+#         file = open(file_name)
+#         content += file.read() + '\n'
+#     return content
 
 def markdown_to_html(md_string):
     return markdown.markdown(md_string, 
                              extensions=['extra'], 
                              output_format='html5')
 
-def html_to_pdf(html_string, output_file):
-    pdfkit.from_string(html_string, output_file)
+def html_to_pdf(html_ary, output_file):
+    options = {
+        'page-size': 'A4',
+        'margin-top': '0.75in',
+        'margin-right': '0.75in',
+        'margin-bottom': '0.75in',
+        'margin-left': '0.75in',
+        'encoding': "UTF-8",
+        'no-outline': None
+    }
+    pdfkit.from_string(html_ary, output_file, options=options, toc={})
+
+def mds_to_pdf(md_file_ary, output_file):
+    htmls = []
+    for md_file in md_file_ary:
+        file = open(md_file)
+        content = file.read()
+        file.close()
+        htmls.append(markdown_to_html(content))
+
+    html_to_pdf(htmls, output_file)
+
 
 if __name__ == '__main__':
-    content = concatenate_file_path_array_to_string(CONSTRUCTIVE_SYSTEM)
-    body = markdown_to_html(content)
-    html_content = HTML_TEMPLATE % {'body': body}
-    html_to_pdf(html_content, 'constructive_bidding_system.pdf')
-    # file = open('constructive_bidding_system.html', 'w')
-    # file.write(html_content)
+    mds_to_pdf(CONSTRUCTIVE_SYSTEM, 'constr.pdf')
+    mds_to_pdf(DEFENSIVE_SYSTEM, 'defense.pdf')
+
